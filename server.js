7a15d6111822a33df673373a53f4231b55003f4b
TotoB12 <totobeliard@outlook.com>
2024-10-02 20:28:33 -0400

    Refactor package.json to add dependencies for msedge-tts and xml-escape

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const axios = require('axios');
-const sharp = require('sharp');
+const { MsEdgeTTS, OUTPUT_FORMAT } = require('msedge-tts');
+const xmlEscape = require('xml-escape');
 require('dotenv').config();
 
 const app = express();
@@ -9,39 +9,6 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-
-app.get("/image/*", async (req, res) => {
-    const imageUrl = decodeURIComponent(req.params[0]);
-    const { p } = req.query;
-
-    try {
-        const imageResponse = await axios.get(imageUrl, {
-            responseType: "stream",
-            headers: {
-                "User-Agent":
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
-                    "(KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0",
-            },
-        });
-
-        const contentType = imageResponse.headers["content-type"];
-        res.set("Content-Type", contentType);
-
-        if (p) {
-            const pixels = parseInt(p, 10);
-
-            const transformer = sharp().resize(pixels, pixels, { fit: 'inside' });
-
-            imageResponse.data.pipe(transformer).pipe(res);
-        } else {
-            imageResponse.data.pipe(res);
-        }
-
-    } catch (error) {
-        console.error("Failed to retrieve the image:", error);
-        res.status(500).send("Failed to retrieve image");
-    }
-});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
